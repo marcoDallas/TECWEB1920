c3866733ac11ced934aecddeb4207f6017c1427b
RiccardoCestaro <riccardocestaro@outlook.it>
2019-12-03 20:37:50 +0100

    #195 aggiunto in print_content la stampa del menu

--- website/www/php/print_content.php
+++ website/www/php/print_content.php
@@ -22,12 +22,55 @@ class Print_content{
         return str_replace("%",$title,file_get_contents("../html/components/header.html"));
     }
 
+    public static function breadcrumb($page){
+        return '<div id="breadcrumb" class="box a_column"><p>Ti trovi in: '.$page.'</p></div>';
+    }
+
+    public static function menu($page){
+        $content='<div id="menu_container" class="thin_column">'."\r".'<div id="menu" class="mobile_hidden" role="navigation">'."\r".'<dl>
+        '."\r".'<dt class="hidden" role="tablist" aria-setsize="5" aria-posinset="1"><a href="#content" tabindex="1">Vai al contenuto</a></dt>';
+        if(!strcmp($page,"home.php")){
+            $content=$content."\r".'<dt xml:lang="en"><strong>Home</strong></dt>'."\r".'
+            <dt role="tablist" aria-setsize="5" aria-posinset="2"><a href="prodotti.php?type=paste" tabindex="2">Paste</a></dt>'."\r".'
+            <dt role="tablist" aria-setsize="5" aria-posinset="3"><a href="prodotti.php?type=torte" tabindex="3">Torte</a></dt>'."\r".'
+            <dt role="tablist" aria-setsize="5" aria-posinset="4"><a href="storia.php" tabindex="4">Storia</a></dt>'."\r".'
+            <dt role="tablist" aria-setsize="5" aria-posinset="5"><a href="contatti.php" tabindex="5">Contatti</a></dt>';
+        }else if(!strcmp($page,"storia.php")){
+            $content=$content."\r".'<dt role="tablist" aria-setsize="5" aria-posinset="2"><a href="home.php" xml:lang="en" tabindex="2">Home</a></dt>'."\r".'
+            <dt role="tablist" aria-setsize="5" aria-posinset="3"><a href="prodotti.php?type=paste" tabindex="2">Paste</a></dt>'."\r".'
+            <dt role="tablist" aria-setsize="5" aria-posinset="4"><a href="prodotti.php?type=torte" tabindex="3">Torte</a></dt>'."\r".'
+            <dt><strong>Storia</strong></dt>'."\r".'
+            <dt role="tablist" aria-setsize="5" aria-posinset="5"><a href="contatti.php" tabindex="5">Contatti</a></dt>';
+        }else if(!strcmp($page,"contatti.php")){
+            $content=$content."\r".'<dt role="tablist" aria-setsize="5" aria-posinset="2"><a href="home.php" xml:lang="en" tabindex="2">Home</a></dt>'."\r".'
+            <dt role="tablist" aria-setsize="5" aria-posinset="3"><a href="prodotti.php?type=paste" tabindex="3">Paste</a></dt>'."\r".'
+            <dt role="tablist" aria-setsize="5" aria-posinset="4"><a href="prodotti.php?type=torte" tabindex="4">Torte</a></dt>'."\r".'
+            <dt role="tablist" aria-setsize="5" aria-posinset="5"><a href="storia.php" tabindex="5">Storia</a></dt>'."\r".'
+            <dt><strong>Contatti</strong></dt>';
+        }else if(!strcmp($page,"prodotti.php?type=paste")){
+            $content=$content."\r".'<dt xml:lang="en" role="tablist" aria-setsize="5" aria-posinset="2"><a href="home.php" tabindex="2">Home</a></dt>'."\r".'
+            <dt><strong>Paste</strong></dt>'."\r".'
+            <dt role="tablist" aria-setsize="5" aria-posinset="3"><a href="prodotti.php?type=torte" tabindex="3">Torte</a></dt>'."\r".'
+            <dt role="tablist" aria-setsize="5" aria-posinset="4"><a href="storia.php" tabindex="4">Storia</a></dt>'."\r".'
+            <dt role="tablist" aria-setsize="5" aria-posinset="5"><a href="contatti.php" tabindex="5">Contatti</a></dt>';
+        }else if(!strcmp($page,"prodotti.php?type=torte")){
+            $content=$content."\r".'<dt role="tablist" aria-setsize="5" aria-posinset="2"><a href="home.php" xml:lang="en" tabindex="2">Home</a></dt>'."\r".'
+            <dt role="tablist" aria-setsize="5" aria-posinset="3"><a href="prodotti.php?type=paste" tabindex="3">Paste</a></dt>'."\r".'
+            <dt><strong>Torte</strong></dt>'."\r".'
+            <dt role="tablist" aria-setsize="5" aria-posinset="4"><a href="storia.php" tabindex="4">Storia</a></dt>'."\r".'
+            <dt role="tablist" aria-setsize="5" aria-posinset="5"><a href="contatti.php" tabindex="5">Contatti</a></dt>';
+        }
+        $content=$content."\r".'</dl>'."\r".'</div>'."\r".'<div id="menu_icon" onclick="toggleMenu(this)">'."\r".'<div class="bar1"></div>'."\r".'
+        <div class="bar2"></div>'."\r".'<div class="bar3"></div>'."\r".'</div>'."\r".'</div>';
+        return $content;
+    }
+
+    
+
     public static function footer(){
         return file_get_contents("../html/components/footer.html");
     }
 
-    public static function breadcrumb($page){
-        return '<div id="breadcrumb" class="box a_column"><p>Ti trovi in: '.$page.'</p></div>'));
-    }
+
 } 
 ?>
