4280a3dfd793b881fca488d9092a136dbf1fd826
RiccardoCestaro <riccardocestaro@outlook.it>
2019-12-09 22:52:16 +0100

    #321 #337 controlli di sicurezza

--- website/www/php/backend/edit_news.php
+++ website/www/php/backend/edit_news.php
@@ -8,6 +8,13 @@ class Edit_news{
 
     public static function edit(){
         if(isset($_POST['submitEditNews']) && isset($_POST['title']) && isset($_POST['description'])){
+            require_once 'backend/input_security_check.php';
+            $title = Input_security_check::general_input_check($_POST['title']);
+            $description = Input_security_check::general_input_check($_POST['description']);
+            if(!$title || !$description){
+                error_log("Security check failed");
+                return FALSE;
+            }
             include_once('backend/get_news.php');
             (new Get_news())->set_news($_POST['title'],$_POST['description']);
         }
