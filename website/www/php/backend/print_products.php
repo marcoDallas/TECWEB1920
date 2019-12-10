9077c61bb277ff2f45225c88cf80775b35471649
RiccardoCestaro <riccardocestaro@outlook.it>
2019-12-10 15:10:01 +0100

    #285 aggiunto avviso nessun prodotto trovato

--- website/www/php/backend/print_products.php
+++ website/www/php/backend/print_products.php
@@ -14,16 +14,6 @@ class Print_products{
     }
 
     public function print_pr($arr){
-        if(!$arr)
-            return;
-        if(!isset($_GET['page']))
-            $_GET['page']=1;
-        if(!array_slice($arr,$_GET['page'].'0'-10,10))
-            $_GET['page']=1;
-        if(sizeof($arr)>10){
-            $arrpage=array_slice($arr,$_GET['page'].'0'-10,10);
-        }else
-            $arrpage=$arr;
         echo('<div class="body_column">');
         echo('<form id="ricercaProdotti" method="get" action="prodotti.php"');
         echo('<label for="cercaProdotti">Cerca '.$_GET['type'].'</label>');
@@ -31,46 +21,58 @@ class Print_products{
         echo('<input type="text" id="cercaProdotti" name="search" tabindex="9"/>');
         echo('<input type="submit" value="search" tabindex="10"/>');
         echo('</form>');
-        if(Admin::verify()){
-            echo('<form method="post" action="modifica_prodotto.php">');
-            echo('<input type="hidden" name="prevpage" value="'.$_SERVER['REQUEST_URI'].'"/>');
-            echo('<input type="submit" name="add" value="Aggiungi Prodotto" tabindex="10"/>');
-            echo('</form>');
-        }
-        echo('<ul>');
-        foreach($arrpage as &$prodotto){
-            echo('<li class="product">');
-            echo('<div class="box full_column element">');
-            echo('<h3>'.$prodotto['Nome'].'</h3>');
-			echo('<div>');
-			echo('<img src="'.$prodotto['Immagine'].'"/>');
-			echo('</div>');
-			echo('<div>');
-            echo('<p>'.$prodotto['Descrizione'].'</p>');
+        if($arr){
+            if(!isset($_GET['page']))
+                $_GET['page']=1;
+            if(!array_slice($arr,$_GET['page'].'0'-10,10))
+                $_GET['page']=1;
+            if(sizeof($arr)>10){
+                $arrpage=array_slice($arr,$_GET['page'].'0'-10,10);
+            }else
+                $arrpage=$arr;
             if(Admin::verify()){
-                echo('<form method="post" action="'.$_SERVER['REQUEST_URI'].'">');
-                echo('<input type="hidden" name="product" value="'.$prodotto['Codice'].'"/>');
-                echo('<input type="submit" name="remove" value="Rimuovi prodotto"/>');
-                echo('</form>');
                 echo('<form method="post" action="modifica_prodotto.php">');
-                echo('<input type="hidden" name="product" value="'.$prodotto['Codice'].'"/>');
                 echo('<input type="hidden" name="prevpage" value="'.$_SERVER['REQUEST_URI'].'"/>');
-                echo('<input type="submit" name="edit" value="Modifica prodotto"/>');
+                echo('<input type="submit" name="add" value="Aggiungi Prodotto" tabindex="10"/>');
                 echo('</form>');
             }
-			echo('</div>');
-            echo('</div>');
-            echo('</li>');
-        }
-        echo('</ul>');
-        if(sizeof($arr)>10){
-            $nextpage=$_GET['page']+1;
-            if(array_slice($arr,$nextpage.'0'-10,10))
-                echo('<a href="?type='.$_GET['type'].'&page='.$nextpage.'">NEXTPAGE</a>');
-            if($_GET['page']>1){
-                $previouspage=$_GET['page']-1;
-                echo('<a href="?type='.$_GET['type'].'&page='.$previouspage.'">PREVIOUSPAGE</a>');
+            echo('<ul>');
+            foreach($arrpage as &$prodotto){
+                echo('<li class="product">');
+                echo('<div class="box full_column element">');
+                echo('<h3>'.$prodotto['Nome'].'</h3>');
+                echo('<div>');
+                echo('<img src="'.$prodotto['Immagine'].'"/>');
+                echo('</div>');
+                echo('<div>');
+                echo('<p>'.$prodotto['Descrizione'].'</p>');
+                if(Admin::verify()){
+                    echo('<form method="post" action="'.$_SERVER['REQUEST_URI'].'">');
+                    echo('<input type="hidden" name="product" value="'.$prodotto['Codice'].'"/>');
+                    echo('<input type="submit" name="remove" value="Rimuovi prodotto"/>');
+                    echo('</form>');
+                    echo('<form method="post" action="modifica_prodotto.php">');
+                    echo('<input type="hidden" name="product" value="'.$prodotto['Codice'].'"/>');
+                    echo('<input type="hidden" name="prevpage" value="'.$_SERVER['REQUEST_URI'].'"/>');
+                    echo('<input type="submit" name="edit" value="Modifica prodotto"/>');
+                    echo('</form>');
+                }
+                echo('</div>');
+                echo('</div>');
+                echo('</li>');
             }
+            echo('</ul>');
+            if(sizeof($arr)>10){
+                $nextpage=$_GET['page']+1;
+                if(array_slice($arr,$nextpage.'0'-10,10))
+                    echo('<a href="?type='.$_GET['type'].'&page='.$nextpage.'">NEXTPAGE</a>');
+                if($_GET['page']>1){
+                    $previouspage=$_GET['page']-1;
+                    echo('<a href="?type='.$_GET['type'].'&page='.$previouspage.'">PREVIOUSPAGE</a>');
+                }
+            }
+        }else{
+            echo('<p>Nessun prodotto trovato</p>');
         }
         echo('</div>');
     }
