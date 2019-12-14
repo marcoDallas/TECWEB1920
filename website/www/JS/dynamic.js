13c2f07399a24146d281bc0cec2827bcdee0f0c8
RiccardoCestaro <riccardo.cestaro@outlook.it>
2019-12-14 09:29:09 +0100

    aggiunta visualizzazione automatica e jquery

--- website/www/JS/dynamic.js
+++ website/www/JS/dynamic.js
@@ -28,3 +28,34 @@ function toggleLogin(icon) {
 
 }
 
+
+function input_image(){
+    document.getElementById("image").onchange = function () {
+        
+        var reader = new FileReader();
+        if(this.files[0].size>528385){
+            alert("Image Size should not be greater than 500Kb");
+            $("#preview").attr("src","blank");
+            $("#preview").hide();  
+            $('#image').wrap('<form>').closest('form').get(0).reset();
+            $('#image').unwrap();     
+            return false;
+        }
+        if(this.files[0].type.indexOf("image")==-1){
+            alert("Invalid Type");
+            $("#preview").attr("src","blank");
+            $("#preview").hide();  
+            $('#image').wrap('<form>').closest('form').get(0).reset();
+            $('#image').unwrap();         
+            return false;
+        }   
+        reader.onload = function (e) {
+            // get loaded data and render thumbnail.
+            document.getElementById("preview").src = e.target.result;
+            $("#preview").show(); 
+        };
+
+        // read the image file as a data URL.
+        reader.readAsDataURL(this.files[0]);
+    };
+};
