046b3236d0c0cddd270da25e78e1241538573953
RiccardoCestaro <riccardo.cestaro@outlook.it>
2019-12-17 21:13:56 +0100

    #426 aggiunti controlli lunghezza massima

--- website/www/php/backend/input_security_check.php
+++ website/www/php/backend/input_security_check.php
@@ -10,7 +10,7 @@ class Input_security_check{
     }
 
     public static function username_check($field){
-        if(strlen($field) == 0)
+        if(strlen($field) == 0 ||  strlen($field) > 20)
             return FALSE;
         $field = Input_security_check::general_controls($field);
         if(preg_match('/[a-z_\-0-9]/i', $field)) //caratteri supportati: a-z A-Z 0-9 - _
@@ -19,7 +19,7 @@ class Input_security_check{
     }
 
     public static function password_check($field){
-        if(strlen($field) == 0)
+        if(strlen($field) == 0  ||  strlen($field) > 20)
             return FALSE;
         $field = Input_security_check::general_controls($field);
         if(preg_match('/[a-z_!?\-0-9]/i', $field)) //caratteri supportati: a-z A-Z 0-9 - _ ! ?
@@ -28,14 +28,14 @@ class Input_security_check{
     }
  
     public static function description_input_check($field){
-        if(strlen($field) < 20)
+        if(strlen($field) < 20  ||  strlen($field) > 500)
             return FALSE;
         $field = Input_security_check::general_controls($field);
         return addslashes($field);
     }
 
     public static function title_input_check($field){
-        if(strlen($field) < 3)
+        if(strlen($field) < 3  ||  strlen($field) > 40)
             return FALSE;
         $field = Input_security_check::general_controls($field);
         return addslashes($field);
