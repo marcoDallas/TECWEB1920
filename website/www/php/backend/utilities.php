b99d44b2ddaa092fd0a330ea7b0612349c87e053
Riccardo Cestaro <riccardo.cestaro@outlook.it>
2020-01-09 18:59:55 +0100

    commentata classe utilities

--- website/www/php/backend/utilities.php
+++ website/www/php/backend/utilities.php
@@ -2,13 +2,14 @@
 
 class Utilities
 {
+    /* Il metodo ritorna il nome della pagina corrente con l'iniziale maiuscola */
     public static function get_page_name()
     {
         $array=explode('/', $_SERVER['PHP_SELF']);
         $page=explode('.', end($array));
         return ucfirst($page[0]);
     }
-
+    /* Il metodo ritorna il nome della pagina, in base a un indirizzo */
     public static function shrink_page($page)
     {
         $array=explode('/', $page);
