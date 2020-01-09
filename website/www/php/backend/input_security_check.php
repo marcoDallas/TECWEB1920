5f41fb745820cd2d0d3ac6a326185b33390769aa
Riccardo Cestaro <riccardo.cestaro@outlook.it>
2020-01-09 18:46:58 +0100

    commentata classe input_security_check

--- website/www/php/backend/input_security_check.php
+++ website/www/php/backend/input_security_check.php
@@ -5,11 +5,12 @@
 
 class Input_security_check
 {
+    /* Il metodo esegue dei controlli generali sui campi per prevenire attacchi sql */
     public static function general_controls($field)
     {
         return trim(htmlentities(strip_tags($field)));
     }
-
+    /* Il metodo esegue dei controlli sul campo username */
     public static function username_check($field)
     {
         if (strlen($field) < 5 ||  strlen($field) > 20) {
@@ -21,7 +22,7 @@ class Input_security_check
         }
         return false;
     }
-
+    /* Il metodo esegue dei controlli sul campo password */
     public static function password_check($field)
     {
         if (strlen($field) < 5  ||  strlen($field) > 20) {
@@ -33,7 +34,7 @@ class Input_security_check
         }
         return false;
     }
- 
+    /* Il metodo esegue dei controlli sul campo description */
     public static function description_input_check($field)
     {
         if (strlen($field) < 20  ||  strlen($field) > 500) {
@@ -44,7 +45,7 @@ class Input_security_check
         $field = Input_security_check::tag_conversion_emph($field);
         return addslashes($field);
     }
-
+    /* Il metodo esegue dei controlli sul campo title */
     public static function title_input_check($field)
     {
         if (strlen($field) < 3  ||  strlen($field) > 40) {
@@ -54,8 +55,7 @@ class Input_security_check
         $field = Input_security_check::tag_conversion_language($field);
         return addslashes($field);
     }
-
-
+    /* Il metodo esegue dei controlli sul campo search */
     public static function search_input_check($field)
     {
         if (strlen($field) > 40) {
@@ -67,8 +67,7 @@ class Input_security_check
         }
         return $field;
     }
-
-
+    /* Il metodo gestisce l'input di lingue */
     public static function tag_conversion_language($field)
     {
         while (preg_match('/\[([A-Za-z]{2}?)=(.*?)\]/', $field, $output)) {
@@ -77,7 +76,7 @@ class Input_security_check
         }
         return $field;
     }
-
+    /* Il metodo gestisce l'input di enfasi */
     public static function tag_conversion_emph($field)
     {
         while (preg_match('/\[\*(.*?)\*\]/', $field, $output)) {
@@ -90,7 +89,7 @@ class Input_security_check
         }
         return $field;
     }
-
+    /* Il metodo controlla i tag e li sostituisce per i campi edit */
     public static function tag_check($field)
     {
         while (preg_match('/<strong>(.*?)<\/strong>/', $field, $output)) {
