c3dbee1464a230f7ea87218803c0bcb6aff531f2
Riccardo Cestaro <riccardo.cestaro@outlook.it>
2020-01-09 18:37:43 +0100

    commentata classe get_admin

--- website/www/php/backend/get_admin.php
+++ website/www/php/backend/get_admin.php
@@ -8,12 +8,12 @@ require_once('database_connection.php');
 class Get_admin
 {
     private $admin='';
-
+    /* Il costruttore crea una connessione al database */
     public function __construct()
     {
         $this->admin = new database_connection();
     }
-
+    /* Il metodo esegue la query per verificare se le credenziali inserite sono corrette */
     public function admin($username, $password)
     {
         return @mysqli_fetch_assoc($this->admin->execute("select * from Utente where Username = '$username' and Pwd = '$password'"));
